f8e1be0696dbe590a7e4a17c50407d2478a666e4
liyifeizzz <1269344752@qq.com>
2024-09-14 14:11:19 +0800

    feat: 添加eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,54 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import {fixupPluginRules} from '@eslint/compat';
+import pluginJs from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+const includedFiles = ['**/*.{js,ts,jsx,tsx}'];
+
+export default [
+    {
+        ignores: [
+            '**/node_modules/',
+            '**/.git/',
+            '**/.idea/',
+            '**/dist/',
+        ],
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    {
+        languageOptions: {
+            parserOptions: {ecmaFeatures: {jsx: true}},
+            globals: {...globals.browser, ...globals.node},
+        },
+        settings: {
+            react: {version: 'detect'},
+        },
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+    ...[pluginJs.configs.recommended, ...tsEslint.configs.recommended, pluginReactConfig].map(config => ({
+        ...config,
+        files: includedFiles,
+    })),
+    {
+        plugins: {
+            '@typescript-eslint': tsEslint.plugin,
+            'react-hooks': fixupPluginRules(pluginReactHooks),
+        },
+        rules: {
+            ...pluginReactHooks.configs.recommended.rules,
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    caughtErrorsIgnorePattern: '^_',
+                    destructuredArrayIgnorePattern: '^_',
+                },
+            ],
+        },
+        files: includedFiles,
     },
-  },
-)
+    eslintConfigPrettier,
+];
